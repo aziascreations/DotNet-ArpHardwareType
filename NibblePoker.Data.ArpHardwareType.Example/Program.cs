b550735499e48cdb8380b596832c0b7a5a8c778d
Herwin <herwin.bozet@gmail.com>
2024-05-08 03:36:09 +0200

    Added example, Finished base readme, Prepared 0.0.1 pre-release Update NibblePoker.Data.ArpHardwareType.Example.csproj, Program.cs, and 4 more files...

---
+++ NibblePoker.Data.ArpHardwareType.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NibblePoker.Data.ArpHardwareType;
+
+namespace NibblePoker.Data.ArpHardwareType.Example {
+    static class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("IANA:");
+            foreach(EIanaArpHardwareTypes hardwareType in Enum.GetValues(typeof(EIanaArpHardwareTypes))) {
+                Console.WriteLine($"* {(ulong) hardwareType} - '{ArpHardwareTypeName.GetFrom(hardwareType)}'");
+            }
+        }
+    }
+}
--- NibblePoker.Data.ArpHardwareType/ArpHardwareTypeName.cs
+++ NibblePoker.Data.ArpHardwareType/ArpHardwareTypeName.cs
@@ -2,24 +2,35 @@
 using System.Reflection;
 
 namespace NibblePoker.Data.ArpHardwareType {
+    /// <summary>
+    ///   Attribute used to give a friendly name to entries in the <i>ARP Hardware Types</i> enumerations.
+    /// </summary>
+    /// <seealso cref="EIanaArpHardwareTypes"/>
+    /// <seealso cref="ELinuxArpHardwareTypes"/>
     public class ArpHardwareTypeName : Attribute {
         private readonly string _description;
     
+        /// <summary>
+        ///   Instantiates an <see cref="Attribute">Attribute</see> that contains a friendly name for any
+        ///    entry in the <i>ARP Hardware Types</i> enumerations.
+        /// </summary>
+        /// <param name="description">Friendly name to be attributed</param>
         public ArpHardwareTypeName(string description) {
             this._description = description;
         }
     
+        /// <summary>
+        ///   Retrieves the friendly name from a given <i>ARP Hardware Types</i>.
+        /// </summary>
+        /// <param name="enumObj">The <i>ARP Hardware Types</i> enumeration member whose friendly name is retrieved.</param>
+        /// <returns>The friendly name if found, ane empty string otherwise.</returns>
         public static string GetFrom(Enum enumObj) {
             FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
         
             if(fieldInfo != null) {
                 object[] attribArray = fieldInfo.GetCustomAttributes(false);
                 if(attribArray.Length != 0) {
-                    /*if(attribArray[0] is ArpHardwareTypeName attrib) {
-                        return attrib.Description;
-                    }*/
-                    ArpHardwareTypeName attrib = attribArray[0] as ArpHardwareTypeName;
-                    if(attrib != null) {
+                    if(attribArray[0] is ArpHardwareTypeName attrib) {
                         return attrib._description;
                     }
                 }
